da58ed7a2974d8517d5d47444089be08cdda03d4
Lucas <lucasfschraier@gmail.com>
2023-10-24 18:50:34 -0400

    feat: handle log in button by dispatching login request

--- src/components/pages/landing/login-form/index.tsx
+++ src/components/pages/landing/login-form/index.tsx
@@ -2,13 +2,30 @@ import { useState } from 'react';
 import * as S from './styled';
 import { handleInput } from '@/utils/state.util';
 import Link from '@/components/UI/common/link';
+import { useLoginMutation } from '@/api/auth.api';
+import { useDispatch } from 'react-redux';
 
 export default function LoginForm({...props}) {
+
+    const dispatch = useDispatch();
+
+    const [triggerLogin, {isLoading}] = useLoginMutation();
 
     const [inputs, setInputs] = useState({
         username: '',
         password: ''
     });
+
+    const handleLogin = () => {
+        triggerLogin({
+            user: inputs
+        })
+        .unwrap()
+        .then( (res) => {
+        })
+        .catch( (err) => {
+        })
+    }
 
     return(
         <S.Root {...props}>
@@ -24,7 +41,9 @@ export default function LoginForm({...props}) {
                 onChange={(e)=>handleInput(e, 'password', inputs, setInputs)}
             />
             <S.FormButton
+                loading={isLoading}
                 disabled={inputs.username.length < 1 || inputs.password.length < 1}
+                onClick={handleLogin}
             >
                 <div>Log In</div>
             </S.FormButton>
