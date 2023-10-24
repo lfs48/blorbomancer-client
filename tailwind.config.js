1d42d0a5e517db243c7d27bee4eaaaaac6f64d5b
Lucas <lucasfschraier@gmail.com>
2023-10-24 17:03:44 -0400

    feat: define color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#191516'
+        },
+        purple: {
+          '50': '#f1f2fc',
+          '100': '#e6e7f9',
+          '200': '#d2d3f3',
+          '300': '#b6b6eb',
+          '400': '#a099e0',
+          '500': '#8c7fd5',
+          '600': '#725ac1',
+          '700': '#6c55ad',
+          '800': '#57478c',
+          '900': '#493f70',
+          '950': '#2b2541',
+      },
+        white: {
+          DEFAULT: '#F4F3EE'
+        }
+      }
+    },
   },
   plugins: [],
 }
