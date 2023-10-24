2c6450ce6a4f8f7e835447706079709791f65891
Lucas <lucasfschraier@gmail.com>
2023-10-24 17:10:41 -0400

    style: apply appwide stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Routes from "@components/routes"
+import Routes from "@components/routes";
+import tw from "tailwind-styled-components";
+
+const Root = tw.div`
+  w-full
+  h-full
+  bg-gradient-to-r
+  from-purple-100
+  to-purple-200
+`
 
 function App() {
 
   return (
-    <Routes />
+    <Root>
+      <Routes />
+    </Root>
   )
 }
 
