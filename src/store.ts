a3e218dab53a5717ce490c91af51816a6e78e4ae
Lucas <lucasfschraier@gmail.com>
2023-10-24 18:32:47 -0400

    feat: add api middleware to store

--- src/store.ts
+++ src/store.ts
@@ -2,23 +2,25 @@
  * Create the store with dynamic reducers
  */
 
- import { configureStore, Middleware } from '@reduxjs/toolkit';
- import logger from 'redux-logger';
- import rootReducer from './reducers/root.reducer';
- 
- export function configureAppStore(preloadedState={}) {
-   // Create the store with middleware
-   const middlewares:Middleware[] = [];
-   if (process.env.NODE_ENV !== 'production') {
-     middlewares.push(logger);
-   }
- 
-   const store = configureStore({
-     reducer: rootReducer,
-     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
-     devTools: process.env.NODE_ENV !== 'production',
-     preloadedState: preloadedState
-   });
- 
-   return store;
- }
+import { configureStore, Middleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import rootReducer from './reducers/root.reducer';
+import { rootApi } from '@/api/root.api';
+
+export function configureAppStore(preloadedState={}) {
+  // Create the store with middleware
+  const middlewares:Middleware[] = [];
+  middlewares.push(rootApi.middleware);
+  if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(logger);
+  }
+
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares),
+    devTools: process.env.NODE_ENV !== 'production',
+    preloadedState: preloadedState
+  });
+
+  return store;
+}
